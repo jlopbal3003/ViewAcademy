297b630a7caa4a299a23f23122a758e0b8f2a24b
Jose <jseantoniolopez@gmail.com>
2024-03-16 17:30:22 +0100

    Librería Showdown (interpretar html) implementada. Título de la aplicación modificada. Código de asistente refactorizado y configurado con el spinner, el formato de texto y el bloqueo de los botones. Diseño del asistente ligeramente modificado.

--- Front/AsistenteEscolar/src/components/asistente-virtual/asistente-virtual.component.ts
+++ Front/AsistenteEscolar/src/components/asistente-virtual/asistente-virtual.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { AsistenteService } from 'src/services/asistente.service';
-
+import { Observable } from 'rxjs';
+declare var showdown: any;
 @Component({
   selector: 'app-asistente-virtual',
   templateUrl: './asistente-virtual.component.html',
@@ -16,7 +17,7 @@ export class AsistenteVirtualComponent {
 
   prompt: string = '';
 
-  //inProgress: boolean = false;
+  isLoading: boolean = true;
 
   constructor(
     protected authService: AuthService,
@@ -26,247 +27,100 @@ export class AsistenteVirtualComponent {
   ngOnInit() {
     if (this.authService.rol === 'alumno') {
       this.prompt = 'General';
-      this.getRespuestaGeneral();
+      this.getRespuesta(this.asistenteService.getRespuestaGeneral());
     } else if (this.authService.rol === 'profesor') {
       this.prompt = 'Profesor';
-      this.getRespuestaProfesor();
+      this.getRespuesta(this.asistenteService.getRespuestaProfesor());
     }
   }
 
-  enviarMensaje() {
+  enviarMensaje(mensaje: string) {
     if (this.prompt == 'General') {
-      this.enviarMensajeGeneral();
+      this.setRespuesta(this.asistenteService.enviarMensajeGeneral(mensaje));
     } else if (this.prompt == 'Profesor') {
-      this.enviarMensajeProfesor();
+      this.setRespuesta(this.asistenteService.enviarMensajeProfesor(mensaje));
     } else if (this.prompt == 'Ingles') {
-      this.enviarMensajeIngles();
+      this.setRespuesta(this.asistenteService.enviarMensajeIngles(mensaje));
     } else if (this.prompt == 'Lengua') {
-      this.enviarMensajeLengua();
+      this.setRespuesta(this.asistenteService.enviarMensajeLengua(mensaje));
     } else if (this.prompt == 'Historia') {
-      this.enviarMensajeHistoria();
+      this.setRespuesta(this.asistenteService.enviarMensajeHistoria(mensaje));
     } else if (this.prompt == 'Mates') {
-      this.enviarMensajeMatematicas();
+      this.setRespuesta(
+        this.asistenteService.enviarMensajeMatematicas(mensaje)
+      );
     }
   }
 
-  getRespuestaProfesor() {
-    this.asistenteService.getRespuestaProfesor().subscribe((data: any[]) => {
+  getRespuesta(metodo: Observable<any>) {
+    metodo.subscribe((data: any[]) => {
       this.respuesta = JSON.stringify(data);
       // Convertir el string en un objeto JavaScript
       let responseObject = JSON.parse(JSON.stringify(data));
       // Obtener el mensaje de respuesta
       let respuesta = responseObject.respuesta;
       // Ahora `respuesta` contiene el mensaje de respuesta
-      this.respuesta = respuesta;
+      this.respuesta = this.interpretarTexto(respuesta);
       console.log(this.respuesta);
+      this.isLoading = false;
     });
   }
 
-  enviarMensajeProfesor() {
+  setRespuesta(metodo: Observable<any>) {
     if (this.mensaje.trim() !== '') {
+      this.isLoading = true;
       this.mensajes.push(this.mensaje);
 
       // Llamar al servicio para enviar el mensaje
-      this.asistenteService.enviarMensajeProfesor(this.mensaje).subscribe(
+      metodo.subscribe(
         (response: any) => {
           console.log('Respuesta del servidor:', response);
-          this.respuestas.push(response.respuesta.substring(4));
+          this.respuestas.push(this.interpretarTexto(response.respuesta.substring(4)));
+          this.isLoading = false;
         },
         (error) => {
           console.error('Error al enviar la solicitud:', error);
+          this.isLoading = false;
         }
       );
 
-      this.mensaje = ''; // Limpiar el textarea después de enviar el mensaje
-    }
-  }
-
-  getRespuestaGeneral() {
-    this.asistenteService.getRespuestaGeneral().subscribe((data: any[]) => {
-      this.respuesta = JSON.stringify(data);
-      // Convertir el string en un objeto JavaScript
-      let responseObject = JSON.parse(JSON.stringify(data));
-      // Obtener el mensaje de respuesta
-      let respuesta = responseObject.respuesta;
-      // Ahora `respuesta` contiene el mensaje de respuesta
-      this.respuesta = respuesta;
-      console.log(this.respuesta);
-    });
-  }
-
-  enviarMensajeGeneral() {
-    if (this.mensaje.trim() !== '') {
-      this.mensajes.push(this.mensaje);
-
-      // Llamar al servicio para enviar el mensaje
-      this.asistenteService.enviarMensajeGeneral(this.mensaje).subscribe(
-        (response: any) => {
-          console.log('Respuesta del servidor:', response);
-          this.respuestas.push(response.respuesta.substring(4));
-        },
-        (error) => {
-          console.error('Error al enviar la solicitud:', error);
-        }
-      );
-
-      this.mensaje = ''; // Limpiar el textarea después de enviar el mensaje
-    }
-  }
-
-  getRespuestaIngles() {
-    this.asistenteService.getRespuestaIngles().subscribe((data: any[]) => {
-      this.respuesta = JSON.stringify(data);
-      // Convertir el string en un objeto JavaScript
-      let responseObject = JSON.parse(JSON.stringify(data));
-      // Obtener el mensaje de respuesta
-      let respuesta = responseObject.respuesta;
-      // Ahora `respuesta` contiene el mensaje de respuesta
-      this.respuesta = respuesta;
-      console.log(this.respuesta);
-    });
-  }
-
-  enviarMensajeIngles() {
-    if (this.mensaje.trim() !== '') {
-      this.mensajes.push(this.mensaje);
-
-      // Llamar al servicio para enviar el mensaje
-      this.asistenteService.enviarMensajeIngles(this.mensaje).subscribe(
-        (response: any) => {
-          console.log('Respuesta del servidor:', response);
-          this.respuestas.push(response.respuesta.substring(4));
-        },
-        (error) => {
-          console.error('Error al enviar la solicitud:', error);
-        }
-      );
-
-      this.mensaje = ''; // Limpiar el textarea después de enviar el mensaje
-    }
-  }
-
-  getRespuestaLengua() {
-    this.asistenteService.getRespuestaLengua().subscribe((data: any[]) => {
-      this.respuesta = JSON.stringify(data);
-      // Convertir el string en un objeto JavaScript
-      let responseObject = JSON.parse(JSON.stringify(data));
-      // Obtener el mensaje de respuesta
-      let respuesta = responseObject.respuesta;
-      // Ahora `respuesta` contiene el mensaje de respuesta
-      this.respuesta = respuesta;
-      console.log(this.respuesta);
-    });
-  }
-
-  enviarMensajeLengua() {
-    if (this.mensaje.trim() !== '') {
-      this.mensajes.push(this.mensaje);
-
-      // Llamar al servicio para enviar el mensaje
-      this.asistenteService.enviarMensajeLengua(this.mensaje).subscribe(
-        (response: any) => {
-          console.log('Respuesta del servidor:', response);
-          this.respuestas.push(response.respuesta.substring(4));
-        },
-        (error) => {
-          console.error('Error al enviar la solicitud:', error);
-        }
-      );
-
-      this.mensaje = ''; // Limpiar el textarea después de enviar el mensaje
-    }
-  }
-
-  getRespuestaHistoria() {
-    this.asistenteService.getRespuestaHistoria().subscribe((data: any[]) => {
-      this.respuesta = JSON.stringify(data);
-      // Convertir el string en un objeto JavaScript
-      let responseObject = JSON.parse(JSON.stringify(data));
-      // Obtener el mensaje de respuesta
-      let respuesta = responseObject.respuesta;
-      // Ahora `respuesta` contiene el mensaje de respuesta
-      this.respuesta = respuesta;
-      console.log(this.respuesta);
-    });
-  }
-
-  enviarMensajeHistoria() {
-    if (this.mensaje.trim() !== '') {
-      this.mensajes.push(this.mensaje);
-
-      // Llamar al servicio para enviar el mensaje
-      this.asistenteService.enviarMensajeHistoria(this.mensaje).subscribe(
-        (response: any) => {
-          console.log('Respuesta del servidor:', response);
-          this.respuestas.push(response.respuesta.substring(4));
-        },
-        (error) => {
-          console.error('Error al enviar la solicitud:', error);
-        }
-      );
-
-      this.mensaje = ''; // Limpiar el textarea después de enviar el mensaje
-    }
-  }
-
-  getRespuestaMatematicas() {
-    this.asistenteService.getRespuestaMatematicas().subscribe((data: any[]) => {
-      this.respuesta = JSON.stringify(data);
-      // Convertir el string en un objeto JavaScript
-      let responseObject = JSON.parse(JSON.stringify(data));
-      // Obtener el mensaje de respuesta
-      let respuesta = responseObject.respuesta;
-      // Ahora `respuesta` contiene el mensaje de respuesta
-      this.respuesta = respuesta;
-      console.log(this.respuesta);
-    });
-  }
-
-  enviarMensajeMatematicas() {
-    if (this.mensaje.trim() !== '') {
-      this.mensajes.push(this.mensaje);
-      // Llamar al servicio para enviar el mensaje
-      this.asistenteService.enviarMensajeMatematicas(this.mensaje).subscribe(
-        (response: any) => {
-          console.log('Respuesta del servidor:', response);
-          this.respuestas.push(response.respuesta.substring(4));
-        },
-        (error) => {
-          console.error('Error al enviar la solicitud:', error);
-        }
-      );
       this.mensaje = ''; // Limpiar el textarea después de enviar el mensaje
     }
   }
 
   promptGeneral() {
+    this.isLoading = true;
     this.prompt = 'General';
     this.vaciarChat();
-    this.getRespuestaGeneral();
+    this.getRespuesta(this.asistenteService.getRespuestaGeneral());
   }
 
   promptIngles() {
+    this.isLoading = true;
     this.prompt = 'Ingles';
     this.vaciarChat();
-    this.getRespuestaIngles();
+    this.getRespuesta(this.asistenteService.getRespuestaIngles());
   }
 
   promptLengua() {
+    this.isLoading = true;
     this.prompt = 'Lengua';
     this.vaciarChat();
-    this.getRespuestaLengua();
+    this.getRespuesta(this.asistenteService.getRespuestaLengua());
   }
 
   promptHistoria() {
+    this.isLoading = true;
     this.prompt = 'Historia';
     this.vaciarChat();
-    this.getRespuestaHistoria();
+    this.getRespuesta(this.asistenteService.getRespuestaHistoria());
   }
 
   promptMatematicas() {
+    this.isLoading = true;
     this.prompt = 'Mates';
     this.vaciarChat();
-    this.getRespuestaMatematicas();
+    this.getRespuesta(this.asistenteService.getRespuestaMatematicas());
   }
 
   vaciarChat() {
@@ -277,11 +131,18 @@ export class AsistenteVirtualComponent {
   }
 
   handleEnterKey(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
+    if (event.key === 'Enter' && !this.isLoading) {
       // Aquí llamas al método que deseas ejecutar
-      this.enviarMensaje();
+      this.enviarMensaje(this.mensaje);
       // Evitar el comportamiento por defecto de la tecla "Enter" en el textarea (salto de línea)
       event.preventDefault();
     }
+  }
+
+  interpretarTexto(respuesta: string) {
+    // Convertir Markdown a HTML
+    let converter = new showdown.Converter(),
+      html = converter.makeHtml(respuesta);
+    return html;
   }
 }
